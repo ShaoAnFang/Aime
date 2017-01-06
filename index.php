31a5d0b1e365114f50d730cb49d80f6c4757de20
ShaoAnFang <ilovet720419@gmail.com>
2017-01-06 11:14:58 +0800

    Update index.php

--- index.php
+++ index.php
@@ -7,12 +7,12 @@ error_reporting(0);
 
 //mysql_connect('localhost','root','root');
 //mysql_connect('db4free.net','aime','720419'); //db4 free
-mysql_connect('sql6.freemysqlhosting.net','sql6152521','9snQHwyntA'); //free db hosting
+mysql_connect('sql6.freemysqlhosting.net','sql6152521','9snQHwyntA') or die('Could not connect to database');//free db hosting
 //mysql_connect('aime.cd4jeronymvi.us-west-2.rds.amazonaws.com','MAPD17','1501user40'); AWS
 
 //mysql_select_db("Aime");
 //mysql_select_db("aimeclark");
-mysql_select_db("sql6152521");
+mysql_select_db("sql6152521") or die('Could not select database');
 
 
 mysql_set_charset("utf8");
